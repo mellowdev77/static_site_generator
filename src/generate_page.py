8821404ff0875e806edefeb2a954cb58a1719584
novais <175879339+mellowdev77@users.noreply.github.com>
2025-05-12 19:22:14 +0100

    added github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -9,7 +9,7 @@ def extract_title(markdown):
 
     raise Exception()
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown_file = open(from_path)
     with open(from_path, 'r') as f:
@@ -21,6 +21,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     template = template.replace("{{ Content }}", html)
     template = template.replace("{{ Title }}", title)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     file_name = str(from_path.split("/")[-1])
     new_path = dest_path + "/" + file_name.replace(".md", ".html")
     if not os.path.exists(dest_path):
@@ -28,12 +30,7 @@ def generate_page(from_path, template_path, dest_path):
     to_file = open(new_path, "w")
     to_file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    if not os.path.exists(dir_path_content):
-        return
-    if not os.path.isdir(dir_path_content):
-        return
-
+def generate_pages_recursive(basepath, dir_path_content, template_path, dest_dir_path):
     if os.path.exists(dir_path_content):
         files_and_folders = os.listdir(dir_path_content)
         for file in files_and_folders:
@@ -41,9 +38,33 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             updated_dest = dest_dir_path + "/" + file
             if os.path.isdir(updated_path):
                 print(f"recursive dir: {updated_path}, dest: {updated_dest}")
-                generate_pages_recursive(updated_path, template_path, updated_dest)
+                generate_pages_recursive(basepath, updated_path, template_path, updated_dest)
             else:
                 print(f"non recursive dir: {updated_path}, dest: {dest_dir_path}")
-                generate_page(updated_path, template_path, dest_dir_path)
+                generate_page(basepath, updated_path, template_path, dest_dir_path)
 
+    return
+
+def copy_static(path, gen_folder):
+    if len(os.listdir(path)) == 0:
+        print("found end")
+        return
+    if not os.path.exists(gen_folder):
+        os.mkdir(gen_folder)
+
+    if os.path.exists(path):
+        files_and_folders = os.listdir(path)
+        for file in files_and_folders:
+            updated_path = f"{path}/{file}"
+            if os.path.isdir(updated_path):
+                copy_static(updated_path, gen_folder)
+            else:
+                new_path = ""
+                if len(path.split("static/", 1)) > 1:
+                    new_path = path.split("static/", 1)[1]
+
+                if not os.path.exists(gen_folder + "/" + new_path):
+                    os.mkdir(gen_folder + "/" + new_path)
+
+                shutil.copy(src=updated_path,dst=gen_folder + "/" + new_path)
     return
--- src/main.py
+++ src/main.py
@@ -1,40 +1,18 @@
 from generate_page import *
-from htmlnode import HTMLNode
-from leafnode import LeafNode
-from textnode import TextNode, TextType
-import unittest
 import os
 import shutil
-
-def copy_static(path):
-    if len(os.listdir(path)) == 0:
-        print("found end")
-        return
-    if not os.path.exists("public"):
-        os.mkdir("public")
-
-    if os.path.exists(path):
-        files_and_folders = os.listdir(path)
-        for file in files_and_folders:
-            updated_path = f"{path}/{file}"
-            if os.path.isdir(updated_path):
-                copy_static(updated_path)
-            else:
-                new_path = ""
-                if len(path.split("static/", 1)) > 1:
-                    new_path = path.split("static/", 1)[1]
-
-                if not os.path.exists("public/" + new_path):
-                    os.mkdir("public/" + new_path)
-
-                shutil.copy(src=updated_path,dst="public/" + new_path)
-    return
+import sys
 
 def main():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    copy_static("static")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[0]
+    if basepath == None:
+        basepath = "/"
+
+    gen_folder = "docs"
+    if os.path.exists(gen_folder):
+        shutil.rmtree(gen_folder)
+    copy_static("static", gen_folder)
+    generate_pages_recursive(basepath, "content", "template.html", gen_folder)
 
 if __name__ == "__main__":
     main()
